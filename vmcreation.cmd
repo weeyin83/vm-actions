a63d5aba6931419ee5c3dc382dfb273fa506f51b
Sarah <sarah@sarahlean.com>
2019-12-24 08:54:40 +0000

    Delete vmcreation.cmd

--- vmcreation.cmd
+++
@@ -1,8 +0,0 @@
-az group create --name rg-githubactions --location eastus
-
-az vm create \
-    --resource-group rg-githubactions \
-    --name myVM \
-    --image win2016datacenter \
-    --admin-username sarah-admin \
-    --admin-password !Verb0se!
