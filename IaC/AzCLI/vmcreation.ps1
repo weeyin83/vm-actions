f0231472e79df1f32d1c248d68490b116a27e3ca
Sarah Lean <sarah@sarahlean.com>
2020-01-04 19:52:17 +0000

    tidy up vmcreation script and securestring change

--- IaC/AzCLI/vmcreation.ps1
+++ IaC/AzCLI/vmcreation.ps1
@@ -35,13 +35,13 @@ param(
     $adminLogin,
 
     [Parameter(Mandatory = $True)]  
-    [string]
+    [SecureString]
     $adminPassword
 )
 
 
 #region Login
-# This logs in a service principal
+# This logs into Azure with a Service Principal Account
 #
 Write-Output "Logging in to Azure with a service principal..."
 az login `
@@ -51,10 +51,11 @@ az login `
     --tenant $servicePrincipalTenantId
 Write-Output "Done"
 Write-Output ""
+#endregion
 
-# This sets the subscription to the subscription I need all my apps to
-# run in
-#
+#region Subscription
+#This sets the subscription the resources will be created in
+
 Write-Output "Setting default azure subscription..."
 az account set `
     --subscription $azureSubscriptionName
@@ -62,33 +63,30 @@ Write-Output "Done"
 Write-Output ""
 #endregion
 
-    #region Create Resource Group
-    # # This creates the resource group used to house the VM
-    # #
-    Write-Output "Creating resource group $resourceGroupName in region $resourceGroupNameRegion..."
-    az group create `
-        --name $resourceGroupName `
-        --location $resourceGroupNameRegion
+#region Create Resource Group
+# This creates the resource group used to house the VM
+Write-Output "Creating resource group $resourceGroupName in region $resourceGroupNameRegion..."
+az group create `
+    --name $resourceGroupName `
+    --location $resourceGroupNameRegion
     Write-Output "Done creating resource group"
     Write-Output ""
-    #endregion
+ #endregion
 
-
-        #region Create VM
-    # Create a VM in the resource group
-    # 
-    Write-Output "Creating VM..."
-    try {
-        az vm create  `
-    --resource-group $resourceGroupName `
-    --name $serverName `
-    --image win2016datacenter `
-    --admin-username $adminLogin `
-    --admin-password $adminPassword
+#region Create VM
+# Create a VM in the resource group
+Write-Output "Creating VM..."
+try {
+    az vm create  `
+        --resource-group $resourceGroupName `
+        --name $serverName `
+        --image win2016datacenter `
+        --admin-username $adminLogin `
+        --admin-password $adminPassword
     }
-    catch {
+catch {
     Write-Output "VM already exists"
     }
-    Write-Output "Done creating VM"
-    Write-Output ""
-    #endregion
+Write-Output "Done creating VM"
+Write-Output ""
+#endregion
