22f6943f90393eea2a18097bad5ec004860a3aae
Sarah Lean <sarah@sarahlean.com>
2020-01-06 09:27:52 +0000

    update vm creation and workflows

--- IaC/AzCLI/vmcreation.ps1
+++ IaC/AzCLI/vmcreation.ps1
@@ -1,5 +1,57 @@
-# This IaC script provisions a VM within Azure
-#
+﻿﻿<#
+.SYNOPSIS
+Deploys a virtual machine in Azure using GitHub Actions
+
+.DESCRIPTION 
+This script will deploy a virtual machine within Azure utilising GitHub Actions as the deployment technology. 
+
+.OUTPUTS
+
+
+.NOTES
+Written by: Sarah Lean
+
+Find me on:
+
+* My Blog:	http://www.techielass.com
+* Twitter:	https://twitter.com/techielass
+* LinkedIn:	http://uk.linkedin.com/in/sazlean
+* GitHub:   https://www.github.com/weeyin83
+
+
+.EXAMPLE
+
+
+Change Log
+V1.00, 06/01/2020 - Initial version
+
+License:
+
+The MIT License (MIT)
+
+Copyright (c) 2017 Sarah Lean
+
+Permission is hereby granted, free of charge, to any person obtaining a copy 
+of this software and associated documentation files (the "Software"), to deal 
+in the Software without restriction, including without limitation the rights 
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
+copies of the Software, and to permit persons to whom the Software is 
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all 
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+DEALINGS IN THE SOFTWARE.
+
+#>
+
+
 [CmdletBinding()]
 param(
     [Parameter(Mandatory = $True)]
